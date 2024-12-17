8935f77ec9b6d2175e4a15f81d804504d97d26f2
azzamazizah <azizah@dilogif.my.id>
2024-12-17 21:12:27 +0700

    inisial

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false,
-    minify: true,
+    minify: 'terser',
+    sourcemap: false
   }
 })
